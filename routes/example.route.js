b8e52bf5ad432ed5c2a6854860cd06c3fa6de03e
Gabriel Andreazza <k.gabriel.andreazza@gmail.com>
2021-10-16 17:20:07 -0300

    🤳

--- routes/example.route.js
+++ routes/example.route.js
@@ -4,18 +4,18 @@ const routePath = '/api/examplePathRoute';
 module.exports = function (app) {
     // >> Método de retorno usando status e json (Recomendado)
     // <base_url>/examplePathRoute/example/get/json :: Method GET
-	app.route(routePath + '/example/get/json').get(controller.exampleGetJson);
+    app.route(routePath + '/example/get/json').get(controller.exampleGetJson);
     // <base_url>/examplePathRoute/example/post/json :: Method POST
-	app.route(routePath + '/example/post/json').post(controller.examplePostJson);
+    app.route(routePath + '/example/post/json').post(controller.examplePostJson);
 
 
     // >> Método simples de retorno
     // <base_url>/examplePathRoute/example/get :: Method GET
     app.route(routePath + '/example/get').get(controller.exampleGet);
     // <base_url>/examplePathRoute/example :: Method POST
-	app.route(routePath + '/example/post').post(controller.examplePost);  
+    app.route(routePath + '/example/post').post(controller.examplePost);
 
 
     // <base_url>/examplePathRoute/example/query :: Method GET
-	app.route(routePath + '/example/query').get(controller.exampleQuery);
+    app.route(routePath + '/example/query').get(controller.exampleQuery);
 }
